cd7c7b1c820750adfb4e4106ee702a1c8694ee0b
R4d1x <r4d1x@1yrxd.local>
2021-05-25 22:40:58 +0700

    boilerplate done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Index = () => {
+    return <div>Welcome to App</div>;
+};
+ReactDOM.render(<Index />, document.getElementById('app'));
